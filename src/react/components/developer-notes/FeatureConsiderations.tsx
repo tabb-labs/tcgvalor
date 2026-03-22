5da6d93868b352ad5607b32ab5e25e73ad71ac21
Michael Tabb <miketabb33@gmail.com>
2026-03-21 20:59:28 -0400

    enhance catalog search experiance

--- src/react/components/developer-notes/FeatureConsiderations.tsx
+++ src/react/components/developer-notes/FeatureConsiderations.tsx
@@ -18,7 +18,6 @@ const makeMarkdown = () => {
   markdown.addTitle('Collection')
   markdown.addBullets([
     'Support sub-collections (Binders). For example: a sub-collection of only Illustrated Pokemon cards.',
-    'Pagination.',
     'Set completion tracker.',
   ])
 
@@ -41,18 +40,8 @@ const makeMarkdown = () => {
     'Ability to follow other users to see collections their collections.',
     'Ability to block other users.',
     'User to user messaging.',
-    'Share collection settings. For example, hide email address.',
+    'Share collection settings.',
     'Add ability to edit profile details like: profile image, name, and email.',
-  ])
-
-  markdown.addTitle('Risk Mitigation')
-  markdown.addBullets([
-    "Store back up data of marketplace and Pokemon blueprints. If the source api disappears, most interactivity will be down. Storing a backup will allow interactivity to continue while I work on adding a new data source. **In this event, user's collections will be persisted, but the ability to add new cards and view prices will down until a solution is put into place.** PS: Adding a backup will also prevent the prices from needing to be reloaded, about a 30 min process, after each production deploy.",
-  ])
-
-  markdown.addTitle('Other')
-  markdown.addBullets([
-    'Add user ranked choice voting system to rapidly get feedback from users to determine which features to focus on next.',
   ])
 
   return markdown.print()
